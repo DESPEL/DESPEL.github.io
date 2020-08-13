fa5f4d83785dd2470ddc399f9668c1e418746a60
despel <52083283+DESPEL@users.noreply.github.com>
2020-08-13 10:21:04 -0700

    Updates

---
+++ precache-manifest.8e1aea14ef35827204a4c8131bfa5d7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fda6e33d06b509ff2b5e64bbf004d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e9223ae81a05faf4300",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e692ea33c77562c507cb",
+    "url": "/static/js/2.a500e56e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a500e56e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9223ae81a05faf4300",
+    "url": "/static/js/main.d6bace6c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
